1fd4e13b59d5f57cedea3cb212f4bb7a58003566
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-06 14:05:14 -0700

    added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      accent: {
+        primary: {
+          DEFAULT: "var(--color-primary)",
+          darker: "var(--color-primary-darker)",
+        },
+        secondary: {
+          DEFAULT: "var(--color-secondary)",
+          darker: "var(--color-secondary-darker)",
+        },
+        tertiary: {
+          DEFAULT: "var(--color-tertiary)",
+          darker: "var(--color-tertiary-darker)",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
