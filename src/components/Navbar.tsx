14ff93ff25650a6d0593b789f5d0c297d7fa4254
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-11 11:14:53 -0700

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { useSession } from "next-auth/client";
 import { useEffect } from "react";
+import Image from "next/image";
+import DiscoWaveLogo from '../../public/disco-wave-logo.svg'
 
 export default function Navbar() {
   const [session, loading] = useSession();
@@ -14,12 +16,13 @@ export default function Navbar() {
   }, [session]);
 
   return (
-    <div className="flex items-center border-b border-gray-400 p-3 font-bold">
+    <div className="flex space-x-8 items-center border-b border-gray-300 px-4 py-2 font-bold">
+      <Image src={DiscoWaveLogo} width={54} height={54}/>
       <div className="flex divide-x divide-gray-400">
         <div className="hover:underline ">
           <a href="#" className="flex items-center space-x-1">
             <p>
-              <AiOutlineHome fontSize="2rem" />
+              <AiOutlineHome fontSize="1.3rem" />
             </p>
 
             <p>Home</p>
@@ -30,11 +33,11 @@ export default function Navbar() {
       <div className="w-full justify-end flex">
         {userPfp ? (
           <img
-            className="w-12 h-12 rounded-full border border-gray-300"
+            className="w-10 h-10 rounded-full border border-gray-300"
             src={session.user.image}
           />
         ) : (
-          <AiOutlineUser className="w-12 h-12 border border-gray-300 rounded-full" />
+          <AiOutlineUser className="w-10 h-10 border border-gray-300 rounded-full" />
         )}
       </div>
     </div>
