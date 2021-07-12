36860fa5901772d305f977ab724ea23879b589d4
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-11 18:41:33 -0700

    added resolver to the current playing song

--- src/pages/api/current_song.ts
+++ src/pages/api/current_song.ts
@@ -1,22 +1,25 @@
 import {getSession} from 'next-auth/client'
 var SpotifyWebApi = require('spotify-web-api-node');
 
-export async function getServerSideProps(context){
-  const {req,res} = context;
-  const session = await getSession({req});
-  spotifyApi.setAccessToken(session.accessToken);
-}
 
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_ID,
   clientSecret: process.env.SPOTIFY_SECRET,
-  redirectUri: 'http://www.example.com/callback'
 });
 
-spotifyApi.getMyCurrentPlayingTrack()
+export default async function resolver(req,res){
+  const session = await getSession({req});
+  spotifyApi.setAccessToken(session.accessToken);
+
+  spotifyApi.getMyCurrentPlayingTrack()
   .then(function(data){
-    console.log('Now playing ' + data.body.item.name)
+    res.send('Now playing ' + data.body.item.name)
   }, function(err) {
-    console.log('Error')
-  });
-export{}
+    res.send('Error')
+});
+}
+
+
+
+
+
