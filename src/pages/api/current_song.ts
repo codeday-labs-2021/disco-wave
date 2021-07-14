5429bdc2f5a3fbf3d8147e36dfb2f3eec5b00ba7
bobby2536 <41354965+bobby2536@users.noreply.github.com>
2021-07-14 15:01:35 -0700

    Update current_song.ts
    Added an extra if/else statement to detect if the user is playing anything or not

--- src/pages/api/current_song.ts
+++ src/pages/api/current_song.ts
@@ -8,15 +8,25 @@ const spotifyApi = new SpotifyWebApi({
 
 export async function getCurrentSong(accessToken) {
   spotifyApi.setAccessToken(accessToken);
-  let data = await spotifyApi.getMyCurrentPlaybackState().then(
-    function (data) {
-      return data.body.item
-    },
-    function (err) {
-      console.log('Something went wrong!', err)
-    },
-  )
-  return data
+  spotifyApi.getMyCurrentPlaybackState().then(async function(data) {
+    // Output items
+    if (data.body && data.body.is_playing) {
+      let data = await spotifyApi.getMyCurrentPlaybackState().then(
+        function (data) {
+          return data.body.item
+        },
+        function (err) {
+          console.log('Something went wrong!', err)
+        },
+      )
+      return data
+    } else {
+      return "User is not playing anything, or doing so in private."
+    }
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+  
 }
 
 export default async function resolver(req, res) {
