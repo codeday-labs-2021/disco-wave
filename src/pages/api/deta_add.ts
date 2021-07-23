cbd169d11a66e0ef0bc8ab65b298d7ede9717fb7
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-22 20:40:28 -0700

    apparently there were build errors i fixed those

--- src/pages/api/deta_add.ts
+++
@@ -1,8 +0,0 @@
-const { Deta } = require('deta');
-const deta = Deta(process.env.DETA_KEY)
-const db = deta.Base('sessions_db'); 
-
-export default async function resolver(req, res) {
-    db.put({name: "alex", age: 77})
-    res.send("data added!")
-  }
--- src/pages/api/deta_get.ts
+++
@@ -1,8 +0,0 @@
-const { Deta } = require('deta');
-const deta = Deta(process.env.DETA_KEY)
-const db = deta.Base('sessions_db'); 
-
-export default async function resolver(req, res) {
-    const data = await db.fetch({})
-    res.send(data)
-  }
