e7dfaf70ece9b22131a42a9ac1792258d4f0d0e4
Shubham Patil <shubhampatilsd@gmail.com>
2021-07-26 18:44:50 -0700

    fixed the error in song suggestions

--- src/pages/api/session/sessionsOperations.ts
+++ src/pages/api/session/sessionsOperations.ts
@@ -1,64 +1,64 @@
-const { Deta } = require('deta');
-const deta = Deta(process.env.DETA_KEY)
-const db = deta.Base('sessions_db'); 
+const { Deta } = require("deta");
+const deta = Deta(process.env.DETA_KEY);
+const db = deta.Base("sessions_db");
 
+const bcrypt = require("bcrypt");
 
-const bcrypt = require('bcrypt')
-
-
-
-
-interface appendData{
-    session_id: string;
-    password: string;
-    song_reqs: string[]
+interface appendData {
+  session_id: string;
+  password: string;
+  song_reqs: string[];
 }
 
-
-
-
-
-export default function resolver(req,res){
-    res.send("Hey wait a minute this place is restricted how did you get here?!\nTake a fish for now: ><>");
+export default function resolver(req, res) {
+  res.send(
+    "Hey wait a minute this place is restricted how did you get here?!\nTake a fish for now: ><>"
+  );
 }
 
-export async function verifySessionID(session_id: string){
-    
-    const res = await db.fetch({"session_id":session_id})
-    if(res.items.length<0){
-        return "No session matched the ID given"
+export async function verifySessionID(session_id: string) {
+  const res = await db.fetch({ session_id: session_id });
+  if (res.items.length < 0) {
+    return "No session matched the ID given";
+  }
+  return res;
+}
+
+export function appendSession(data: appendData): void {
+  db.put(data);
+}
+
+export async function appendSong(
+  session_id: string,
+  suggestion: string,
+  password: string
+) {
+  const res = await db.fetch({ session_id: session_id });
+  if (res.items.length > 0) {
+    if (!bcrypt.compareSync(password, res.items[0].password)) {
+      return "Wrong password";
     }
-    return status;
-}
 
-export function appendSession(data:appendData): void{
-    db.put(data);
-    
-}
+    await db.update(
+      { song_reqs: db.util.append(suggestion) },
+      res.items[0].key
+    );
 
-export async function appendSong(session_id: string, suggestion: string, password: string) {
-    
-    const res = await db.fetch({"session_id":session_id})
-    if(res.items.length>0){
-        if(!bcrypt.compareSync(password, res.items[0].password)){return "Wrong password"}
-
-        await db.update({"song_reqs":db.util.append(suggestion)}, res.items[0].key)
-        
-        return "Suggestion added"
-    }else{
-        return "No session matched the ID given"
-    }
+    return "Suggestion added";
+  } else {
+    return "No session matched the ID given";
+  }
 }
-    
 
 export async function getSessions(session_id: string, password: string) {
-    const res = await db.fetch({"session_id":session_id})
-    if(res.items.length>0){
-        if(!bcrypt.compareSync(password, res.items[0].password)){return "Wrong password"}
-        
-        return res.items[0].song_reqs
-    }else{
-        return "No session matched ID given"
+  const res = await db.fetch({ session_id: session_id });
+  if (res.items.length > 0) {
+    if (!bcrypt.compareSync(password, res.items[0].password)) {
+      return "Wrong password";
     }
-    
+
+    return res.items[0].song_reqs;
+  } else {
+    return "No session matched ID given";
+  }
 }
